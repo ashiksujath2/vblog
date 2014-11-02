fa77b1f34190b6b578d7bfe8349dafac55f8e2b7
Ashik Sujath <ashiksujath2@gmail.com>
2014-11-02 14:49:47 +0530

    Models added

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from blog.models import Author, BlogEntry
 
-# Register your models here.
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+class BlogEntryAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(BlogEntry, BlogEntryAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,38 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class BaseModel(models.Model):
+    """Things which we always need to keep track of."""
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Author(models.Model):
+    user = models.ForeignKey(User)
+    about = models.TextField(blank=True)
+
+
+    class Meta:
+        verbose_name = 'Author'
+        verbose_name_plural = 'Authors'
+
+    def __unicode__(self):
+        return self.user.get_full_name()
+
+
+class BlogEntry(BaseModel):
+    title = models.CharField(max_length=300, help_text='Title of the article')
+    slug = models.SlugField(max_length=300)
+    author = models.ForeignKey(Author)
+    is_published = models.BooleanField(default=False, help_text='Only Published Entries will appear in the blog')
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        ordering = ['-created_on']
+        verbose_name_plural = 'Blog Entries'
+
+    def __unicode__(self):
+        return self.title
