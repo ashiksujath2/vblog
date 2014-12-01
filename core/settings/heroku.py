9764962e4e4e7d65d70ffc3ba8f09b0df20a0291
Ashik Sujath <ashiksujath2@gmail.com>
2014-12-01 12:06:55 +0530

    debug set to false

--- core/settings/heroku.py
+++ core/settings/heroku.py
@@ -1,5 +1,9 @@
 from .base import *
 
+DEBUG = False
+
+TEMPLATE_DEBUG = DEBUG
+
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
